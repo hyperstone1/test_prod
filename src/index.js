bc134647162070932328468e3eb27cbb2779d5a6
hyperstone1 <kingofthegorgon@mail.ru>
2023-10-11 04:22:25 +0300

    fixed range slider

--- src/index.js
+++ src/index.js
@@ -18,8 +18,7 @@ import { Navigation, Pagination, Mousewheel } from 'swiper/modules';
 
 const blogTop = document.querySelector('.blog__top');
 const filterBtn = document.querySelectorAll('.category__list-filter');
-const rangeSlider = document.querySelector('.filter__price-range');
-const inputs = document.querySelectorAll('.filter__price-inp');
+const rangeSliders = document.querySelectorAll('.filter__price-range');
 
 const blogCountCurrent = document.querySelector('.blog__count-current');
 const blogCountLast = document.querySelector('.blog__count-last');
@@ -74,67 +73,70 @@ filterBtn &&
     });
   });
 
-if (rangeSlider) {
-  var sliderInstance = noUiSlider.create(rangeSlider, {
-    start: [67000, 521000],
-    // step: [1],
-    connect: true,
-    tooltips: true,
-    range: {
-      min: [0],
-      max: [521000],
-    },
-    format: {
-      to: function (value) {
-        return parseInt(value).toLocaleString('ru-RU') + '';
+if (rangeSliders.length > 0) {
+  rangeSliders.forEach((rangeSlider) => {
+    const inputs = rangeSlider.closest('.filter__price').querySelectorAll('.filter__price-inp');
+    var sliderInstance = noUiSlider.create(rangeSlider, {
+      start: [67000, 401000],
+      // step: [1],
+      connect: true,
+      tooltips: true,
+      range: {
+        min: [0],
+        max: [521000],
       },
-      from: function (value) {
-        return value.replace('', '');
+      format: {
+        to: function (value) {
+          return parseInt(value).toLocaleString('ru-RU') + '';
+        },
+        from: function (value) {
+          return value.replace('', '');
+        },
       },
-    },
-  });
-  const tooltips = document.querySelectorAll('.noUi-tooltip');
-  rangeSlider.noUiSlider.on('update', function (values, handle) {
-    inputs[handle].value = values[handle];
-    console.log('handle: ', tooltips[1]);
-    var percentage =
-      ((parseFloat(values[handle]) - sliderInstance.options.range.min) /
-        (sliderInstance.options.range.max - sliderInstance.options.range.min)) *
-      100000;
-    console.log('percentage: ', percentage);
-    // Если ползунок находится в промежутке от 80% до 100%
-    if (percentage >= 90) {
-      // Вычисляем новое значение left в зависимости от процента и ширины родительского блока
-      var leftValue = -60 + (percentage - 90); // 1.5 - это коэффициент, на который вы хотите уменьшить left
-      tooltips[1].style.left = leftValue + '%';
-    } else {
-      // Сбрасываем стиль left, если ползунок находится вне промежутка от 80% до 100%
-      tooltips[1].style.left = '50%';
-    }
-  });
-
-  inputs.forEach(function (input, handle) {
-    input.addEventListener('input', function () {
-      const value = this.value;
-      const selectionStart = this.selectionStart;
-      const prevValueLength = value.length;
-      const newValue = value
-        .replace(/[^0-9]/g, '')
-        .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
-        .toLocaleString('ru-RU');
-      const diff = newValue.length - prevValueLength;
-      this.value = newValue;
-
-      // Корректировка позиции курсора при удалении первого символа
-      if (diff < 0 && selectionStart <= newValue.indexOf(' ')) {
-        this.setSelectionRange(newValue.indexOf(' ') + 1, newValue.indexOf(' ') + 1);
+    });
+    const tooltips = document.querySelectorAll('.noUi-tooltip');
+    rangeSlider.noUiSlider.on('update', function (values, handle) {
+      inputs[handle].value = values[handle];
+      console.log('handle: ', tooltips[1]);
+      var percentage =
+        ((parseFloat(values[handle]) - sliderInstance.options.range.min) /
+          (sliderInstance.options.range.max - sliderInstance.options.range.min)) *
+        100000;
+      console.log('percentage: ', percentage);
+      // Если ползунок находится в промежутке от 80% до 100%
+      if (percentage >= 90) {
+        // Вычисляем новое значение left в зависимости от процента и ширины родительского блока
+        var leftValue = -60 + (percentage - 90); // 1.5 - это коэффициент, на который вы хотите уменьшить left
+        tooltips[1].style.left = leftValue + '%';
       } else {
-        this.setSelectionRange(selectionStart + diff, selectionStart + diff);
+        // Сбрасываем стиль left, если ползунок находится вне промежутка от 80% до 100%
+        tooltips[1].style.left = '50%';
       }
     });
-    input.addEventListener('change', function () {
-      console.log(this.value.replace(/\s/g, ''));
-      rangeSlider.noUiSlider.setHandle(handle, this.value.replace(/\s/g, ''));
+
+    inputs.forEach(function (input, handle) {
+      input.addEventListener('input', function () {
+        const value = this.value;
+        const selectionStart = this.selectionStart;
+        const prevValueLength = value.length;
+        const newValue = value
+          .replace(/[^0-9]/g, '')
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+          .toLocaleString('ru-RU');
+        const diff = newValue.length - prevValueLength;
+        this.value = newValue;
+
+        // Корректировка позиции курсора при удалении первого символа
+        if (diff < 0 && selectionStart <= newValue.indexOf(' ')) {
+          this.setSelectionRange(newValue.indexOf(' ') + 1, newValue.indexOf(' ') + 1);
+        } else {
+          this.setSelectionRange(selectionStart + diff, selectionStart + diff);
+        }
+      });
+      input.addEventListener('change', function () {
+        console.log(this.value.replace(/\s/g, ''));
+        rangeSlider.noUiSlider.setHandle(handle, this.value.replace(/\s/g, ''));
+      });
     });
   });
 }
